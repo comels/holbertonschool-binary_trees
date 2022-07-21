216207ae215933c41cc3176fa6dbe8d421622275
Come Le Sauter <come.l.s@hotmail.fr>
2022-07-21 15:34:27 +0200

    101

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -2,7 +2,6 @@
 
 void print_level(const binary_tree_t *tree, size_t level, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
-void print_order(const binary_tree_t *tree, void (*func)(int));
 
 /**
  * binary_tree_height - function that measures the height of a binary tree
